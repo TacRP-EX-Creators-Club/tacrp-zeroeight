ee58dc2b0cb164c2b0380b335aede9ad26555bf0
VinierAardvark1 <jackprovine@gmail.com>
2026-04-08 22:50:37 -0500

    The Localizationing Splittification
    Split localization into multiple files, one per pack. The main TacRP mod no longer needs to be updated to make changes to separate packs.

---
+++ lua/tacrp/shared/langs/content_zeroeight_sv-se.lua
@@ -0,0 +1,12 @@
+L = {} -- Swedish Content Strings by Moka
+
+///////////////////// -- [[ One-Offs - Zero Eight ]] --
+-- Weapons
+ws = "tacrp_sp_"
+w = ws .. "mp5_zeroeight"
+L["wep." .. w .. ".name.full"] = "MP5/10 \"Zero Eight\""
+L["wep." .. w .. ".name"] = "Zero Eight"
+L["wep." .. w .. ".desc"] = "Anpassad 10 mm MP5:a med Swordfish-kit, prototyp framgrepp och trummagasin. Tung mynningsbroms förbättrar rekylhanteringen och ser tuff ut också."
+L["wep." .. w .. ".desc.quote"] = "\"Läxan till dig är att inte ens pröva.\""
+L["wep." .. w .. ".trivia.manufacturer"] = "Neptunium Arms"
+L["wep." .. w .. ".credits"] = "Speciellt redigerad av speedonerd och 8Z \nMP5: Mr. Brightside, Stoke, Twinkie Masta, FxDarkloki \nTillbehör: Treyarch, BlackSpot Entertainment, Crytek \nLjud: Strelok, CS:O2"
---
+++ lua/tacrp/shared/langs/content_zeroeight_zh-cn.lua
@@ -0,0 +1,12 @@
+L = {} -- Chinese Content Strings by 8Z
+
+///////////////////// -- [[ One-Offs - Zero Eight ]] --
+-- Weapons
+ws = "tacrp_sp_"
+w = ws .. "mp5_zeroeight"
+-- L["wep." .. w .. ".name.full"] = "MP5/10 \"Zero Eight\""
+-- L["wep." .. w .. ".name"] = "Zero Eight"
+-- L["wep." .. w .. ".desc"] = "Customized 10mm MP5 with Swordfish kit, prototype foregrip and drum magazine. Weighted muzzle brake improves recoil handling and smacks real hard too."
+-- L["wep." .. w .. ".desc.quote"] = "\"The lesson for you is never try.\""
+-- L["wep." .. w .. ".trivia.manufacturer"] = "Neptunium Arms"
+-- L["wep." .. w .. ".credits"] = "Custom edited by speedonerd and 8Z \nMP5: Mr. Brightside, Stoke, Twinkie Masta, FxDarkloki \nAccessories: Treyarch, BlackSpot Entertainment, Crytek \nSounds: Strelok, CS:O2"
